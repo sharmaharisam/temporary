f09c72310779642090e53350e8fd34a22c9060db
sharmaharisam <54685523+sharmaharisam@users.noreply.github.com>
2020-11-21 17:55:39 +0530

    Update A1List.java

--- A1List.java
+++ A1List.java
@@ -32,35 +32,35 @@ public class A1List extends List {
 
     public boolean Delete(Dictionary d) 
     {
-        A1List curr = this.getFirst();
-        if(curr == null) return false;
-        while(curr.next!=null)
+        A1List curr = this.getFirst(); // start from the first node
+        if(curr == null) return false; // if list is empty, there is nothing to return
+        while(curr.next!=null) // // loop through the list
         {
-            if((curr.key == d.key) && (curr.address == d.address) && (curr.size == d.size))
+            if((curr.key == d.key) && (curr.address == d.address) && (curr.size == d.size)) // check for a match
             {
-                curr.prev.next = curr.next;
-                curr.next.prev = curr.prev;
+                curr.prev.next = curr.next; // elementary pointer fix
+                curr.next.prev = curr.prev; // elementary pointer fix
                 return true;
             }
             curr = curr.next;
         }
-        return false;
+        return false; // return false if search is unsuccessfull
     }
 
     public A1List Find(int k, boolean exact)
     {
-        A1List curr = this.getFirst();
-        if(curr == null) return null;
-        if(exact == true)
+        A1List curr = this.getFirst(); // start from the first node 
+        if(curr == null) return null; // if list is empty, there is nothing to return
+        if(exact == true) // for the case of exact matching
         {
-            while(curr.key != -1)
+            while(curr.next != null) // loop through the list
             {
-                if(curr.key == k)return curr;
+                if(curr.key == k)return curr; // check for match 
                 curr = curr.next;
             }
-            return null;
+            return null; // return null in case of unsuccessfull search
         }
-        else
+        else // the case of first fit search
         {
             while(curr.key != -1)
             {
@@ -78,22 +78,66 @@ public class A1List extends List {
         {
             curr = curr.prev;
         }
-        if(curr.next.next == null)return null;
+        if(curr.next.next == null)return null; // if list is empty, return null
         return curr.next; // return the first/head node
     }
     
     public A1List getNext() 
     {
-        if (this.next.next == null)
+        if (this.next.next == null)// check if the current node id the last node
         {
-            return this.getFirst();
+            return this.getFirst(); // in case of the last node, return the head node
         }
-        return this.next;
+        return this.next; // return the next node
     }
 
     public boolean sanity()
     {
+        if( this.checkLoops() == true ) return false;
+
+        A1List curr = this;
+        while(true)
+        {
+            if(curr.next.prev != curr)return false; // this is a necessary condition for each node
+            if(curr.prev.next != curr)return false; // this is a necessary condition for each node
+            if(curr.key == -1)break; // break when you reach the head sentinel
+            curr = curr.prev;
+        }
+        A1List headSentinel = curr;
+        if(headSentinel.prev != null) return false; // the prev of the head sentinel should be null
+        
+        curr = this;
+        while(true)
+        {
+            if(curr.next.prev != curr)return false; // this is a necessary condition for each node
+            if(curr.prev.next != curr)return false; // this is a necessary condition for each node
+            if(curr.key == -1)break; // break when you reach the tail sentinel
+            curr= curr.next;
+        }
+        if(curr.next != null)return false; // the next of the tail sentinel should be null
+
         return true;
+    }
+
+    private boolean checkLoops()
+    {
+        A1List p1 = this;
+        A1List p2 = this;
+        while(p1.next != null && p2.next != null && p2.next.next != null)
+        {
+            p1 = p1.next;
+            p2 = p2.next.next;
+            if(p1 == p2)return true;
+        }
+        p1 = this;
+        p2 = this;
+        while(p1.prev != null && p2.prev != null && p2.prev.prev != null)
+        {
+            p1 = p1.prev;
+            p2 = p2.prev;
+            if(p1 == p2)return true;
+        }
+        return false;
     }
 
 }
